2af4f0bbbca261b855335c31c872fd2839069906
luizventurote <luiz@randwmedia.com>
2016-10-15 19:23:49 -0300

    Adds a new talk about Magento Development

---
+++ desenvolvimento-de-lojas-virtuais-com-magento/README.md
@@ -0,0 +1,1 @@
+# Desenvolvimento de lojas virtuais com Magento
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Talks - Luiz Venturote</title>
+</head>
+<body>
+
+	<?php
+
+		$dir_open = opendir('.');
+
+		while(false !== ($filename = readdir($dir_open))){
+		    if($filename != "." && $filename != ".." && $filename != ".git" && $filename != "index.php" && $filename != "README.md") {
+		        $link = "<a href='./$filename'> $filename </a>";
+		        echo $link;
+		    }
+		}
+
+		closedir($dir_open);
+
+	?>
+
+	<style>
+		
+		a {
+			color: #4c4c4c;
+		    padding: 15px 10px;
+		    display: block;
+		    margin: 0;
+		    border-bottom: 1px solid #dedede;
+		    text-decoration: none;
+		    font-family: Arial;
+		    text-transform: uppercase;
+		    font-size: 12px;
+		}
+
+		a:hover {
+			background: #eee;
+		}
+
+	</style>
+	
+</body>
+</html>
